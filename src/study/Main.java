2e40bfb541b411ea5a660d3bc52dfb1c5dfea62a
J-Branch <jamalbranch01@gmail.com>
2023-11-03 15:50:11 -0400

    added some imports and main method

--- src/study/Main.java
+++ src/study/Main.java
@@ -1,5 +1,23 @@
 package study;
 
-public class Main {
-    
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
 }
