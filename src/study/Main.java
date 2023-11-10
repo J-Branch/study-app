5c65e911fd22f8d09fd611b81320136f2df2e035
J-Branch <jamalbranch01@gmail.com>
2023-11-10 00:55:29 -0500

    got the buttons in the right spot, and added a clock

--- src/study/Main.java
+++ src/study/Main.java
@@ -1,6 +1,7 @@
 package study;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Group;
@@ -10,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 
@@ -22,21 +24,42 @@ public class Main extends Application
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //this creates the buttons
-        Button ccards = new Button("create cards");
-        Button csets = new Button("card sets");
+        Button cardSetsButton = new Button("Card sets");
+        Button createSetButton = new Button("Create Set");
+        Label clock = new Label("12:00");
 
-        Label welcome = new Label("welcome");
 
-        csets.setPrefSize(100, 50);
-        ccards.setPrefSize(100, 50);
+        BorderPane root = new BorderPane();// the main layout container to add everything else to
+        HBox buttonBox = new HBox();// striclty for the buttons
+        VBox clockBox = new VBox();// layout for the clock
 
-        HBox root = new HBox(20,ccards, csets);
-        root.getChildren().add(welcome);
+    //                    BUTTONS/ BUTTONBOX
+        //----------------------------------------------------------------
+        cardSetsButton.setPrefSize(100, 70);
+        createSetButton.setPrefSize(100, 70);
+
+        buttonBox.getChildren().addAll(createSetButton, cardSetsButton);
+        buttonBox.setSpacing(30);// set spacing for the spacing inbetween the buttons
+        buttonBox.setAlignment(Pos.CENTER);// sets the alignment of the buttonBox to the center of the region
+        Insets buttonBoxpPadding = new Insets(0, 0, 100, 0);// padding for buttonBox
+        BorderPane.setMargin(buttonBox, buttonBoxpPadding);// sets the buttonBox padding inside the root
+        root.setBottom(buttonBox);
+        //----------------------------------------------------------------
+
+    //                  CLOCK
+        //----------------------------------------------------------------
+        clock.setPrefSize(100, 70);
+        clockBox.getChildren().add(clock);
+        clockBox.setAlignment(Pos.TOP_RIGHT);
+        Insets clockBoxPadding = new Insets(50, 0, 0, 0);
+        BorderPane.setMargin(clockBox, clockBoxPadding);
+        root.setTop(clockBox);
+        //----------------------------------------------------------------
+
         root.setBackground(Background.EMPTY);
-        root.setAlignment(Pos.CENTER);
+        buttonBox.setBackground(Background.EMPTY);
 
-        Scene menu = new Scene(root, 600, 600, Color.BLANCHEDALMOND);
+        Scene menu = new Scene(root, 600, 600, Color.LIGHTBLUE);
         primaryStage.setScene(menu);
         primaryStage.setTitle("Study App");
         primaryStage.show();
